d2d7e15989f49eb6b52f105bcdace8131c184d1b
Panya Batphantana <fanneioz@yahoo.co.th>
2025-04-16 20:23:15 +0700

    feat: Summary output printer

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.24
 
 require github.com/shopspring/decimal v1.4.0
 
-require github.com/opn-ooo/challenges v0.0.0-20240913094335-d0c3dabe14c6 // indirect
+require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/opn-ooo/challenges v0.0.0-20240913094335-d0c3dabe14c6 // indirect
+)
---
+++ internal/presenter/console/tonpahpa_summary_printer.go
@@ -0,0 +1,22 @@
+package console
+
+import (
+	"fmt"
+	"github.com/dustin/go-humanize"
+	"go-tamboon/internal/domain/donation"
+	"go-tamboon/shared/money"
+	"strings"
+)
+
+func PrintTonPahPaSummary(summary donation.TonPahPaSummary) {
+	printLine := func(label string, inputAmount money.Money) {
+		amount := humanize.FormatFloat("#,###.##", inputAmount.Amount())
+		fmt.Printf("%20s: %-5s %15s\n", label, strings.ToUpper(inputAmount.Currency()), amount)
+	}
+
+	printLine("total received", summary.Total)
+	printLine("successfully donated", summary.Successful)
+	printLine("faulty donation", summary.Faulty)
+	fmt.Println()
+	printLine("average per person", summary.Average)
+}
